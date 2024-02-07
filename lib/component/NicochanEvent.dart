b0a759e5c39ec7172af81dd499c717865ee33976
JC-21\210277 <210277@jc-21.jp>
2024-02-07 17:03:54 +0900

    おまけ要素の実装

--- lib/component/NicochanEvent.dart
+++ lib/component/NicochanEvent.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class StateNicochanEvent extends StatefulWidget{
 
@@ -9,19 +10,22 @@ class StateNicochanEvent extends StatefulWidget{
 }
 
 class NicochanEvent extends State<StateNicochanEvent> {
+  double x = Random().nextInt (100).toDouble()-50;
+  double y = Random().nextInt (150).toDouble()-70;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        height: 100,
-        width: 100,
-        decoration: ShapeDecoration(
-            color: Colors.transparent,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0))),
-        child: Image.asset('images/nicochan.png')
-      ),
+       child: Container(
+           alignment:  Alignment(x,y),
+           height: 100,
+           width: 100,
+           decoration: ShapeDecoration(
+               color: Colors.transparent,
+               shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(15.0))),
+           child: Image.asset('images/nicochan.png')
+       ),
     );
   }
 }
