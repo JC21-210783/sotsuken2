e9039d5d06322d63c1df7c042ced26bd717ae3db
JC21-210277 <210277@jc-21.jp>
2024-02-08 00:00:46 +0900

    おまけ機能がなんとなくできた

--- lib/component/NicochanEvent.dart
+++ lib/component/NicochanEvent.dart
@@ -10,22 +10,36 @@ class StateNicochanEvent extends StatefulWidget{
 }
 
 class NicochanEvent extends State<StateNicochanEvent> {
-  double x = Random().nextInt (100).toDouble()-50;
-  double y = Random().nextInt (150).toDouble()-70;
+  double x = (Random().nextInt (16).toDouble()-8)/10;
+  double y = (Random().nextInt (16).toDouble()-8)/10;
+  double rotate = Random().nextInt (4).toDouble()-2;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-       child: Container(
-           alignment:  Alignment(x,y),
-           height: 100,
-           width: 100,
-           decoration: ShapeDecoration(
-               color: Colors.transparent,
-               shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.circular(15.0))),
-           child: Image.asset('images/nicochan.png')
-       ),
+        child:GestureDetector(
+        onTap: (){
+          debugPrint(x.toString() + ',' + y.toString());
+          showDialog(
+              barrierColor:Colors.transparent,
+              context: context,
+              builder: (_){
+                return StateNicochanEvent();
+              }
+          );
+        },
+         child: Container(
+            alignment:  Alignment(x,y),
+            child: Transform.rotate(
+              angle: rotate,
+              child: const Image(
+                image: AssetImage('images/nicochan.png',),
+                width: 100,
+                height: 100,
+              )
+            ),
+         ),
+        ),
     );
   }
 }
