41907602b685b01aeac63dd875e0eeed81bdbbba
JC-21\210277 <210277@jc-21.jp>
2024-02-13 17:35:05 +0900

    debugPrint削除

--- lib/ui/ChooseUser.dart
+++ lib/ui/ChooseUser.dart
@@ -46,7 +46,6 @@ class ChooseUser_Page extends State<StateChooseUser>{
     Timer.periodic(Duration(milliseconds: 25), (Timer timer) {
       setState(() {
         ++counter;
-        debugPrint('counterのなかみ$counter');
         if(counter < 12){
           _value += (0.005 * counter/2);
         }else if(counter > 20){
